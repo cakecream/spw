bb65aa3436c351b171d94dae8da3d18e399713bf
cakecream <khanomcake_cream@hotmail.com>
2015-04-16 00:01:29 +0700

    *02* เพิ่มปุ่มควบคุมและเปลี่ยนสี

--- f2/spw/GamePanel.java
+++ f2/spw/GamePanel.java
@@ -17,13 +17,13 @@ public class GamePanel extends JPanel {
 	public GamePanel() {
 		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
 		big = (Graphics2D) bi.getGraphics();
-		big.setBackground(Color.BLACK);
+		big.setBackground(Color.ORANGE);
 	}
 
 	public void updateGameUI(GameReporter reporter){
 		big.clearRect(0, 0, 400, 600);
 		
-		big.setColor(Color.PINK);		
+		big.setColor(Color.BLACK);		
 		big.drawString(String.format("%04d", reporter.getScore()), 300, 20);
 		for(Sprite s : sprites){
 			s.draw(big);
